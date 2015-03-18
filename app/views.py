7a6f4a6df72e7b88fe5e30e7cbd4d6ee75c2ac36
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-03-18 18:53:08 +0300

    Add index page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
-from flask import abort, make_response, render_template
+from flask import jsonify, abort, request, make_response, render_template
 from app import app, auth, db, models
+
+def allTeams(fameous):
+	query = models.Team.query
+	if fameous:
+		query = query.filter(models.Team.imageUrl != None)
+	return query.all()
 
 @app.errorhandler(404)
 def not_found(error):
@@ -8,3 +14,9 @@ def not_found(error):
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/teams')
+def getTeams():
+	query = request.args['query']
+	teams = [{'id':team.id, 'name': team.name} for team in models.Team.query.filter(models.Team.name.contains(query), models.Team.imageUrl != None).order_by('name').all()]
+	return jsonify(items = teams)
